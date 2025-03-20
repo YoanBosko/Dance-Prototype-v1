98ce4eea2d9a176d91e337f9b33e831ad04f1e79
YoanBosko <yoanbosko@gmail.com>
2025-03-21 01:39:00 +0700

    Initial commit

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static ScoreManager Instance;
+    public AudioSource hitSFX;
+    public AudioSource missSFX;
+    public TMPro.TextMeshPro scoreText;
+    public TMPro.TextMeshPro resultText;
+    static int comboScore;
+    static string result;
+    void Start()
+    {
+        Instance = this;
+        comboScore = 0;
+        result = "";
+    }
+    public static void Perfect()
+    {
+        result = "Perfect";
+        comboScore += 1;
+        Instance.hitSFX.Play();
+    }
+    public static void Good()
+    {
+        result = "Godo";
+        comboScore += 1;
+        Instance.hitSFX.Play();
+    }
+    public static void Bad()
+    {
+        result = "Bad";
+        comboScore = 0;
+        Instance.hitSFX.Play();
+    }
+    public static void Miss()
+    {
+        result = "Miss";
+        comboScore = 0;
+        Instance.missSFX.Play();    
+    }
+    private void Update()
+    {
+        resultText.text = result;
+        scoreText.text = comboScore.ToString();
+    }
+}
